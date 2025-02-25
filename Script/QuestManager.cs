fa7e2b281ad39268c4ae3c814ebda078640e572f
matheusbruno-git <84660523+matheusbruno-git@users.noreply.github.com>
2025-02-25 17:39:04 -0300

    Update QuestManager.cs

--- Script/QuestManager.cs
+++ Script/QuestManager.cs
@@ -1,8 +1,68 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestManager : MonoBehaviour
+public enum MissionState
 {
-    public Quest[] gameQuests;
+    Inactive,
+    Active,
+    Completed
+}
+
+[CreateAssetMenu(fileName = "NewQuest", menuName = "Scriptables/Quest", order = 3)]
+public class Quest : ScriptableObject
+{
+    public string QuestName;
+    [TextArea] public string QuestDescription;
+
+    [Header("Quest Conditions")]
+    public List<QuestCondition> questConditions;
+
+    [Header("Quest Status")]
+    public MissionState missionState = MissionState.Inactive;
+
+    [Header("Related NPCs")]
+    public NPC_Behaviour[] EmittingNPCs;
+
+    public void ActivateQuest()
+    {
+        if (missionState == MissionState.Inactive)
+        {
+            missionState = MissionState.Active;
+            Debug.Log($"Quest '{QuestName}' is now Active.");
+        }
+    }
+
+    public bool CheckCompletion(PlayerData playerData)
+    {
+        if (missionState == MissionState.Active)
+        {
+            foreach (QuestCondition condition in questConditions)
+            {
+                if (!condition.IsConditionMet(playerData))
+                {
+                    return false;
+                }
+            }
+
+            CompleteQuest();
+            return true;
+        }
+        return false;
+    }
+
+    private void CompleteQuest()
+    {
+        if (missionState == MissionState.Active)
+        {
+            missionState = MissionState.Completed;
+            Debug.Log($"Quest '{QuestName}' completed.");
+        }
+    }
+
+    public void ResetQuest()
+    {
+        missionState = MissionState.Inactive;
+        Debug.Log($"Quest '{QuestName}' has been reset to Inactive.");
+    }
 }
